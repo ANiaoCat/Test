f9bff854062c4be68e2bc2f2ca7a8e3454b8bc5a
ANiaoCat <15201171103@163.com>
2018-03-27 23:22:09 +0800

    wechat

---
+++ 微信/pages/food/paySucceed.js
@@ -0,0 +1,72 @@
+// pages/food/paySucceed.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    orderList: [{ foodName: "酱香牛排", price: "50.00", nums: 1 }, { foodName: "港式奶茶", price: "12.00", nums: 1 }, { foodName: "番茄炒蛋", price: "18.00", nums: 1 }, { foodName: "鱼香肉丝", price: "22.00", nums: 1 }, { foodName: "鱼香肉丝", price: "22.00", nums: 1 }, { foodName: "红烧肉", price: "45.00", nums: 1 }, { foodName: "巴西炒饭", price: "23.00", nums: 1 }, { foodName: "糖醋里脊", price: "30.00", nums: 1 }, { foodName: "鱼香肉丝", price: "44.00", nums: 2 }, { foodName: "酱香牛排", price: "50.00", nums: 1 }, { foodName: "酱香牛排", price: "50.00", nums: 1 }, { foodName: "酱香牛排", price: "50.00", nums: 1 }, { foodName: "酱香牛排", price: "50.00", nums: 1 }, { foodName: "酱香牛排", price: "50.00", nums: 1 }, { foodName: "酱香牛排", price: "50.00", nums: 1 }]
+  },
+  // 返回首页
+  comeHome:function(e){},
+  // 取消支付
+  payCancel:function(e){},
+  // 微信支付
+  wPay:function(e){},
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
